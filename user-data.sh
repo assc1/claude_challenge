ed711da107d9bf27de565b1e262239c917c329f8
assc1 <jb4378@gmail.com>
2022-03-29 00:34:06 -0500

    initial commit

---
+++ user-data.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+apt-get update
+apt-get install -y apache2
+service apache2 start
+echo "" > /var/www/html/index.html
+cat <<-EOF > /var/www/html/index.html
+<!DOCTYPE html>
+
+<html>
+	<head>
+		<title>
+			First Web Page
+		</title>
+	</head>
+
+	<body>
+		Hello World!
+	</body>
+</html>
+EOF
